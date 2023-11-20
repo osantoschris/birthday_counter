fb82d75390117ec782604587214c5bf7654f0dc8
Christian Oliveira <52898213+osantoschris@users.noreply.github.com>
2023-11-19 22:43:32 -0300

    update the date

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 import Countdown from "./coutndown.js";
 
-const tempoParaOWWB = new Countdown("08 november 2023 00:01:00 GMT-0300");
+const tempoParaOWWB = new Countdown("01 december 2023 07:00:00 GMT-0300");
 const tempos = document.querySelectorAll("[data-time]");
 
 function mostrarTempo() {
